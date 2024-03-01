cd3338c6ae862323ee27727d1088fd5c7a263cce
Mr-Yash-01 <mr.yashvadukiya.2003@gmail.com>
2024-03-01 09:55:53 +0530

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+task("accounts","Print",async (task,hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts){
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.24",
+  defaultNetwork:"sepoliaa",
+  networks:{
+    hardhat:{},
+    sepoliaa:{
+      url:"https://sepolia.infura.io/v3/cdf2a29bbe4a45218e91aaaa2aa87b9a",
+      accounts:["5c93769363cbb7a9abbbdfeae15f18fa44d0cfe08e64332d491fc11b10b366f0"]
+    }
+  }
+};
