a91e9b00f169775201b65218ac786d0580832cc2
Mr-Yash-01 <mr.yashvadukiya.2003@gmail.com>
2024-03-03 15:31:59 +0530

    Hardhat run

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,10 +12,10 @@ task("accounts","Print",async (task,hre) => {
 
 module.exports = {
   solidity: "0.8.24",
-  defaultNetwork:"sepoliaa",
+  defaultNetwork:"sepolia",
   networks:{
     hardhat:{},
-    sepoliaa:{
+    sepolia:{
       url:"https://sepolia.infura.io/v3/cdf2a29bbe4a45218e91aaaa2aa87b9a",
       accounts:["5c93769363cbb7a9abbbdfeae15f18fa44d0cfe08e64332d491fc11b10b366f0"]
     }
